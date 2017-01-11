bab2b5d64889869864bac8afd3ee4f7a805f23eb
wullvill <wullvill@163.com>
2017-01-11 18:03:26 +0800

    first commit project

---
+++ src/main/java/com/ailk/eaap/integration/o2p_parent_maven/App.java
@@ -0,0 +1,13 @@
+package com.ailk.eaap.integration.o2p_parent_maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
